90a817018f69ada273f98c79a7fa33940dd68af3
gr1hsa <st088141@student.spbu.ru>
2023-04-02 14:39:16 +0300

    fjsdfj

--- First lab/main.py
+++ First lab/main.py
@@ -19,35 +19,50 @@ import matplotlib.pyplot as plt
 #
 # def f(x: float) -> float:
 #     return 1 - x / 3
+# def p(x):
+#     return (5 - x) / (7 - 3 * x)
+#
+#
+# def q(x):
+#     return - (1 - x) / 2
+#
+#
+# def r(x):
+#     return 1 + 1 / 2 * sin(x)
+#
+#
+# def f(x):
+#     return 1 / 2 + x / 2
+
 def p(x):
-    return (5 - x) / (7 - 3 * x)
+    return 1 / (2 + x)
 
 
 def q(x):
-    return - (1 - x) / 2
+    return 0
 
 
 def r(x):
-    return 1 + 1 / 2 * sin(x)
+    return cos(x)
 
 
 def f(x):
-    return 1 / 2 + x / 2
+    return 1 + x
 
 
 def main():
     print("Вариант 9")
     print("Дифференциальное уравнение: -u''(6 + x) / ( 7 + 3x) - u'(1 - x / 2) + u(1 + 0.5cos(x)) = 1 - x/3")
-
+    mode = 2
     while True:
         n = 5
         a = -1
         b = 1
         h = (b - a) / n
-        alpha1 = 0
-        alpha2 = -1
-        betta1 = 3
-        betta2 = 2
+        alpha1 = 1
+        alpha2 = 0
+        betta1 = 1
+        betta2 = 0
         alpha = 0
         betta = 0
         solves = []
@@ -71,21 +86,24 @@ def main():
                 p_val = p(x)
                 q_val = q(x)
                 r_val = r(x)
-                A = (-p_val / (h ** 2)) - q_val / (2 * h)
-                B = -(2 * p_val / (h ** 2))  - r_val
-                C = (-p_val / (h ** 2)) + q_val / (2 * h)
+                if mode == 1:
+                    A = (-p_val / (h ** 2)) - q_val / (2 * h)
+                    B = -(2 * p_val / (h ** 2)) - r_val
+                    C = (-p_val / (h ** 2)) + q_val / (2 * h)
+                else:
+                    A = -p(a - h / 2 + h * (i - 1 / 2)) / h ** 2 - q_val / (2 * h)
+                    B = -p(a - h / 2 + h * (i + 1 / 2)) / h ** 2 - p(a - h / 2 + h * (i - 1 / 2)) / h**2 - r_val
+                    C = -p(a - h / 2 + h * (i + 1 / 2)) / h ** 2 + q_val / (2 * h)
                 s.append(C / (B - A * s[i - 1]))
                 t.append((A * t[i - 1] - b[i]) / (B - A * s[i - 1]))
             graphic.append(1)
             s.append(0)
-            t.append(((betta1 / 2 - betta2 / h) * t[n] - betta) / ((betta1 / 2 + betta2 / h) - (betta1 / 2 - betta2 / h) * s[n]))
-
+            t.append(((betta1 / 2 - betta2 / h) * t[n] - betta) / (-(betta1 / 2 + betta2 / h) -(betta1 / 2 - betta2 / h) * s[n]))
             #Вычисление y на основе посчитанных s и t
             y = [0] * (n + 2)
             y[n + 1] = t[n + 1]
             for i in range(n, -1, -1):
-                y[i] = (s[i] * y[i + 1] + t[i])
-
+                y[i] = s[i] * y[i + 1] + t[i]
             if len(solves) == 2: #После того, как просчитали два решение вычисляем ошибку
                 er = 0
                 for i in range(0, len(solves[1])):
@@ -97,20 +115,21 @@ def main():
                 er = er ** (1 / 2)
                 error = er
                 dots[0].append(error)
-                dots[1].append(log(n))
+                dots[1].append(n)
 
             if len(solves) == 2:
-                solves[0], solves[1] = solves[1], y
+                solves[0], solves[1] = solves[1], list.copy(y)
             else:
-                solves.append(y)
+                solves.append(list.copy(y))
 
             n *= 2
             h /= 2
         print(f"Итоговая погрешность: {dots[0][-1]}")
-        plt.plot(graphic, y)
+        plt.plot(graphic, solves[-1])
         plt.show()
         plt.plot(dots[1], dots[0])
         plt.show()
+        # print(solves[-1])
 
 
 if __name__ == '__main__':
