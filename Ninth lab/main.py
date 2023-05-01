5f89bb06e3d6163bf56669f7ff2723f20b27e5cd
gr1hsa <st088141@student.spbu.ru>
2023-05-01 11:29:14 +0300

    9 done)

--- Ninth lab/main.py
+++ Ninth lab/main.py
@@ -5,6 +5,10 @@ from matplotlib import cm
 from scipy import linalg
 
 
+def f(x: float, t: float) -> float:
+    return x ** 3 + t ** 3
+
+
 def explicit_schema(num_x: int, num_time: int, x_linspace: list[float], t_linspace: list[float],
                     tau: float, h: float, c: float, mu, mu1, mu2, f):
     u = np.zeros((num_x, num_time))
@@ -35,19 +39,13 @@ def implicit_schema(num_x: int, num_time: int, x_linspace: list[float], t_linspa
         u[0][k] = mu1(t_linspace[k])
         u[-1][k] = mu2(t_linspace[k])
 
-    A = np.zeros((num_x - 2, num_x - 2), dtype=np.float64)
+    A = np.zeros((3, num_x - 2), dtype=np.float64)
 
     cf = c * tau / (h ** 2)
-    A[0][0] = 1 + 2 * cf
-    A[0][1] = -cf
 
-    for i in range(1, num_x - 3):
-        A[i][i - 1] = -cf
-        A[i][i] = 1 + 2 * cf
-        A[i][i + 1] = -cf
-
-    A[-1][-2] = 1 + 2 * cf
-    A[-1][-1] = -cf
+    A[0, 1:] = -cf
+    A[1, :] = 1 + 2 * cf
+    A[2, :-1] = -cf
 
     for k in range(1, num_time):
         b = np.zeros(num_x - 2, dtype=np.float64)
@@ -57,32 +55,30 @@ def implicit_schema(num_x: int, num_time: int, x_linspace: list[float], t_linspa
             b[i] = tau * f(x_linspace[i + 1], t_linspace[k]) + u[i + 1][k - 1]
 
         b[-1] = u[-2][k - 1] + cf * u[-1][k] + tau * f(x_linspace[-2], t_linspace[k])
-        u[1:-1, k] = linalg.solve(A, b)
+        u[1:-1, k] = linalg.solve_banded((1, 1), A, b)
 
     return u
 
 
-def main():
-    c = 1e-1
-    f = lambda x, t: 0
-    mu1 = lambda t: t
-    mu2 = lambda t: t
-    mu = lambda x: x
+def test_implicit():
+    a = 1e-3
+    mu1 = lambda t: sin(t)
+    mu2 = lambda t: sin(t)
+    mu = lambda x: sin(x)
 
     num_x = 200
-    num_time = 1000
+    num_time = 100
 
     x_linspace, h = np.linspace(0, 3, num_x, retstep=True)
     t_linspace, tau = np.linspace(0, 1, num_time, retstep=True)
 
-    kappa = (h ** 2) / (2 * c) - tau
-    print(f"{kappa=}")
 
-    U_implicit = implicit_schema(num_x, num_time, x_linspace, t_linspace, tau, h, c, mu, mu1, mu2, f)
+    print(f"(h ** 2) / (2 * a) - tau = {(h ** 2) / (2 * a) - tau}")
+
+    U_implicit = implicit_schema(num_x, num_time, x_linspace, t_linspace, tau, h, a, mu, mu1, mu2, f)
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
-    # # Make data.
     X, T = np.meshgrid(x_linspace, t_linspace, indexing='ij')
     ax.set_ylabel('time')
     ax.set_xlabel('X')
@@ -92,19 +88,46 @@ def main():
 
     plt.show()
 
-    U_exclipt = explicit_schema(num_x, num_time, x_linspace, t_linspace, tau, h, c, mu, mu1, mu2, f)
+
+def test(explicit=True, bad=False):
+    a = 1e-3
+
+    mu1 = lambda t: sin(t)
+    mu2 = lambda t: sin(t)
+    mu = lambda x: sin(x)
+
+    num_x = 200
+    num_time = 100
+    if bad:
+        num_x = 2000
+    x_linspace, h = np.linspace(0, 3, num_x, retstep=True)
+    t_linspace, tau = np.linspace(0, 1, num_time, retstep=True)
+
+    print(f"(h ** 2) / (2 * a) - tau = {(h ** 2) / (2 * a) - tau}")
+
+    if explicit:
+        U = explicit_schema(num_x, num_time, x_linspace, t_linspace, tau, h, a, mu, mu1, mu2, f)
+    else:
+        U = implicit_schema(num_x, num_time, x_linspace, t_linspace, tau, h, a, mu, mu1, mu2, f)
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
-    # # Make data.
     X, T = np.meshgrid(x_linspace, t_linspace, indexing='ij')
     ax.set_ylabel('time')
     ax.set_xlabel('X')
-    surf = ax.plot_surface(X, T, U_exclipt, cmap=cm.coolwarm,
+    surf = ax.plot_surface(X, T, U, cmap=cm.coolwarm,
                            linewidth=0, antialiased=False)
     fig.colorbar(surf, shrink=0.5, aspect=5)
 
     plt.show()
+
+
+def main():
+
+    test()
+    test(bad=True)
+    test(explicit=False)
+    test(explicit=False, bad=True)
 
 
 if __name__ == '__main__':
